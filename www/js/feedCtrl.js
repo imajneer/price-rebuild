61ebc46c82459f4061503a1f3f7fd8314095c780
Nadeem Yasin <nadeem@westagilelabs.com>
2016-03-19 19:36:05 +0500

    Reverting back the signin page check

--- www/js/feedCtrl.js
+++ www/js/feedCtrl.js
@@ -4,8 +4,8 @@ angular.module('app.feedCtrl',['app.services','ngLodash','ngCordova'])
 	 console.log('loaded feed controller...');
     $scope.$on('$ionicView.beforeEnter',function() {
         console.log('before enter...');
-				if(true){
-        // if(localStorageService.get('accessToken')) {
+				// if(true){
+        if(localStorageService.get('accessToken')) {
 	        //should already be signed in
         } else if(ionic.Platform.isIOS() || ionic.Platform.isAndroid())  {
             $state.go('signin'); //this is commented out to support web dev
